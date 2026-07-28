1fb0a2a8f60039e923e32a6a457d85739cd1228a
joaopaulodevv <joaopaulo.solopes@gmail.com>
2026-07-28 14:16:01 -0300

    shorturl unique constraint added

---
+++ src/Shortener/Migrations/20260728165848_shortcodeuniqueness.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shortener.Migrations
+{
+    /// <inheritdoc />
+    public partial class shortcodeuniqueness : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ShortUrls_ShortCode",
+                table: "ShortUrls",
+                column: "ShortCode",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ShortUrls_ShortCode",
+                table: "ShortUrls");
+        }
+    }
+}
--- src/Shortener/Models/ShortUrl.cs
+++ src/Shortener/Models/ShortUrl.cs
@@ -1,6 +1,9 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace Shortener.Models;
+
+[Index(nameof(ShortCode), IsUnique = true)]
 public class ShortUrl
 {
     [Key]
